b1c3c10e6e86ba63c9254288a1b162c912a8d242
vellayawning <95587397+vellayawning@users.noreply.github.com>
2024-02-06 23:15:48 +0530

    Day 14 - Random Choice Picker - 06-02-2024

---
+++ day14_random-chooice-picker/script.js
@@ -0,0 +1,71 @@
+const tagsel = document.getElementById('tags')
+const textarea = document.getElementById('textarea')
+
+
+textarea.focus()
+
+textarea.addEventListener('keyup', (e) => {
+    createTags(e.target.value)
+
+    if(e.key == 'Enter') {
+        setTimeout(() => {
+            e.target.value = '' 
+        }, 10)
+        
+        randomSelect()
+    }
+})
+
+
+function createTags(input) {
+    console.log(input)
+    const tags = input.split(";").filter(tag => tag.trim() !== '').map(tag => tag.trim())
+    console.log(tags)
+
+    tagsel.innerHTML = ''
+
+    tags.forEach(tag => {
+        const tagel = document.createElement('span')
+        tagel.classList.add('tag')
+        tagel.innerHTML = tag
+        tagsel.appendChild(tagel)
+    })
+
+}
+
+function randomSelect() {
+    const times = 30
+
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag()
+        highlightTag(randomTag)
+
+        setTimeout(() => {
+            unHighlightTag(randomTag)
+        },100)
+    }, 100);
+
+    setTimeout(() => {
+        clearInterval(interval)
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag()
+
+            highlightTag(randomTag)
+        }, 100)
+
+    }, times * 100)
+}
+
+function pickRandomTag() {
+    const tags = document.querySelectorAll('.tag')
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highlightTag(tag) {
+    tag.classList.add('highlight')
+}
+
+function unHighlightTag(tag) {
+    tag.classList.remove('highlight')
+}
