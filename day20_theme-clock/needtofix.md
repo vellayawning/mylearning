e96813999a4a6e4288fce0b1c09cdcec2672bcd3
vellayawning <95587397+vellayawning@users.noreply.github.com>
2024-02-22 23:39:33 +0530

    Day 20.4 - Theme Cloack  v0.4 - 22-02-2024

---
+++ day20_theme-clock/needtofix.md
@@ -0,0 +1,3 @@
+something wrong, till script.js light and dark toggle was wrong,
+
+Now no time and no issue, need to fix, check if variable are declared and called properly.
--- day20_theme-clock/script.js
+++ day20_theme-clock/script.js
@@ -21,3 +21,36 @@ toggle .addEventListener('click', (e) => {
         e.target.innerHTML = 'Light Mode'
     }
 })
+
+
+function setTime() {
+    const time = new Date();
+    const month = time.getMonth()
+    const day = time.getDay()
+    const date = time.getDate()
+    const hours = time.getHours()
+    const hoursForClock = hours % 12
+    const minutes = time.getMinutes()
+    const seconds = time.getSeconds()
+    const ampm = hours >= 12 ? 'PM' : 'AM'
+
+    hourEL.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg);`
+    minuteEL.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg);`
+    secondEL.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg);`
+
+    timeEL.innerHTML = `${hours}:${minutes < 10 ? `0{minutes}` : minutes} ${ampm}`
+
+    dateEL.innerHTML = `${days[day]}, ${months[month]} <span class="circles">${date}</span>"`
+
+
+}
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min  
+}
+
+setTime()
+
+setInterval(setTime, 1000)
+
