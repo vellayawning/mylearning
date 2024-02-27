79bc86147bdabb90148d013277a999b4238aa4dd
vellayawning <95587397+vellayawning@users.noreply.github.com>
2024-02-27 22:49:52 +0530

    Day 23.2 Drawing App v0.2 & Day 24 Kinetic LOader v1.0 - 27-02-2024

--- day23_drawing-app/script.js
+++ day23_drawing-app/script.js
@@ -1,9 +1,48 @@
 //https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
 
 let size = 20
+let isPressed = false
 let color = 'black'
+let x
+let y
+
+canvas.addEventListener('mousedown', (e) =>{
+    isPressed = true
+
+    x = e.offsetX
+    y = e.offsetY
+
+    //console.log(isPressed, x, y)
+})
+
+canvas.addEventListener('mouseup', (e) =>{
+    isPressed = false
+
+    x = undefined
+    y = undefined
+
+    //console.log(isPressed, x, y)
+})
+
+canvas.addEventListener('mousemove', (e) =>{
+    if(isPressed) {
+        const x2 = e.offsetX
+        const y2 = e.offsetY
+
+        drawCircle(x2, y2)
+        drawLine(x, y, x2, y2)
+        
+        x = x2
+        y = y2
+    }
+
+    //console.log(isPressed, x, y)
+})
+
+
 
 //https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Drawing_shapes
 
@@ -19,10 +58,13 @@ function drawLine(x1, y1, x2, y2) {
     ctx.moveTo(x1, y1)
     ctx.lineTo(x2, y2)
     ctx.strokeStyle = color
-    ctx.lineWidth = size
+    ctx.lineWidth = size *2
+    //ctx.lineWidth = size
     ctx.stroke()
 
 }
 
-drawCircle(100, 200)
-drawLine(300, 300, 300, 500)
+//drawCircle(100, 200)
+//drawLine(300, 300, 300, 500)
+
+//
---
+++ day24_kinetic-loader/needfix.md
@@ -0,0 +1,6 @@
+why is the second fan missing, the script was follwed to T.
+
+[Solved] : missed `.`
+
+Before:
+`kinetic::before` for which the fix was `.kinetic::before`
